65bb64a4d54c53fa9afb9bfbdf215553c85b6f5a
ngoytrylyhuor <lyhourkhe123@gmail.com>
2025-03-02 09:22:36 +0700

    no message

--- resources/views/frontend/components/services.blade.php
+++ resources/views/frontend/components/services.blade.php
@@ -10,7 +10,7 @@
                         <h3 class="h6 fw-bold text-black">Interior Design</h3>
                         <p class="text-black-50">Creatively transforming spaces with aesthetics, functionality, and comfort to enhance everyday living.</p>
                         <p>
-                            <a href="{{ route('service_details','interior-design') }}" class="more-2">Learn more<span class="icon-arrow_forward"></span></a>
+                            <a href="{{ route('service_details','interior-design') }}" class="more-2">More Details<span class="icon-arrow_forward"></span></a>
                         </p>
                     </div>
                 </div>
@@ -24,7 +24,7 @@
                         <h3 class="h6 fw-bold text-black">Landscape Design</h3>
                         <p class="text-black-50">Crafting harmonious outdoor spaces that blend nature, beauty, and functionality seamlessly.</p>
                         <p>
-                            <a href="{{ route('service_details','landscape-design') }}" class="more-2">Learn more<span class="icon-arrow_forward"></span></a>
+                            <a href="{{ route('service_details','landscape-design') }}" class="more-2">More Details<span class="icon-arrow_forward"></span></a>
                         </p>
                     </div>
                 </div>
@@ -38,7 +38,7 @@
                         <h3 class="h6 fw-bold text-black">Architecture Design</h3>
                         <p class="text-black-50">Innovating structural concepts with precision, balance, and artistry to shape inspiring environments.</p>
                         <p>
-                            <a href="{{ route('service_details','architecture-design') }}" class="more-2">Learn more<span class="icon-arrow_forward"></span></a>
+                            <a href="{{ route('service_details','architecture-design') }}" class="more-2">More Details<span class="icon-arrow_forward"></span></a>
                         </p>
                     </div>
                 </div>
@@ -52,7 +52,7 @@
                         <h3 class="h6 fw-bold text-black">Floor Plan</h3>
                         <p class="text-black-50">Strategically mapping spatial layouts for efficiency, flow, and optimal interior design solutions.</p>
                         <p>
-                            <a href="{{ route('service_details','floor-plan') }}" class="more-2">Learn more<span class="icon-arrow_forward"></span></a>
+                            <a href="{{ route('service_details','floor-plan') }}" class="more-2">More Details<span class="icon-arrow_forward"></span></a>
                         </p>
                     </div>
                 </div>
